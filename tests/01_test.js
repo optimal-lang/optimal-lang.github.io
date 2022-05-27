3e0736aa01ce0fa2e67cd5f2996cc0bd283a8730
JavaCommons Technologies <javacommons@gmail.com>
2022-05-27 22:46:58 +0900

    .

---
+++ tests/01_test.js
@@ -0,0 +1,28 @@
+// https://deno.land/std@0.141.0/testing#usage
+import {
+  assert,
+  assertFalse,
+  assertEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+
+import { optiMAL } from "../optiMAL2.mjs";
+
+Deno.test("01A", () => {
+  var glob = optiMAL(window);
+  var v1 = glob.runAll(`(+ 11 22]`);
+  console.log(v1);
+  assert(v1 == 33);
+});
+
+Deno.test("01B", () => {
+  var glob = optiMAL(window);
+  var v1 = glob.runAll(`
+    (define x 123)
+    (begin
+      (set! x (+ 1 x))
+      (set! x (+ 2 x))
+      x)
+  `);
+  console.log(v1);
+  assert(v1 == 126);
+});
