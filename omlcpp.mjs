a6277fb9667a4589495471bdb47c23f70c2b6c63
JavaCommons Technologies <javacommons@gmail.com>
2022-06-02 21:41:00 +0900

    .

--- omlcpp.mjs
+++ omlcpp.mjs
@@ -407,7 +407,7 @@ function compile_ast(ast) {
             let condition = compile_body1(ast[1]);
             if (ast[0] === "until")
                 condition = "!" + condition;
-            return ("(([&]()->void {while(" +
+            return ("(([=]()->void {while(" +
                 condition +
                 "){" +
                 compile_body(ast, 2) +
