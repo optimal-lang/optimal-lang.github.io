568c0795e4106802a7d6132db1a4a69f7d439267
JavaCommons Technologies <javacommons@gmail.com>
2022-06-03 02:02:49 +0900

    .

--- omlcpp.h
+++ omlcpp.h
@@ -4,10 +4,27 @@
 
 class oml_root: public gc_cleanup
 {
+/*
 public:
     double value;
     oml_root(double n) : value(n)
     {
+    }
+*/
+public:
+    virtual double number_value() = 0;
+};
+
+class oml_number: public oml_root
+{
+    double value;
+public:
+    oml_number(double n) : value(n)
+    {
+    }
+    virtual double number_value()
+    {
+        return this->value;
     }
 };
 
@@ -15,13 +32,14 @@ static oml_root* undefined = (oml_root*)-1;
 
 static inline oml_root* new_number(double n)
 {
-    return new(GC) oml_root(n);
+    return new(GC) oml_number(n);
 }
 
 static inline double number_value(oml_root* n)
 {
+    if (n==nullptr) return 0;
     if (n==undefined) return 0;
-    return n==nullptr ? 0 : n->value;
+    return n->number_value();
 }
 
 oml_root* console_log(oml_root* x)
