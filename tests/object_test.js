176364581f0843eec5332bc4fc5eac3d74d1ef8c
JavaCommons Technologies <javacommons@gmail.com>
2022-06-12 17:23:59 +0900

    .

--- tests/object_test.js
+++ tests/object_test.js
@@ -15,3 +15,11 @@ Deno.test("OBJECT_A", () => {
   console.log(v1);
   assertEquals(v1, '( 11 22 33 & (":key1" true) (":key2" 1.23) (":key3" undefined) )');
 });
+
+Deno.test("OBJECT_B", () => {
+  var v1 = run(`
+(ast2oml (object & (:key2 1.23) (:key1 true) (:key3 undefined]
+  `);
+  console.log(v1);
+  assertEquals(v1, '( & (":key1" true) (":key2" 1.23) (":key3" undefined) )');
+});
