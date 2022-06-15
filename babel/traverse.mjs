9795d49ba3dfae93f250ceaea32ccde32bbb1489
JavaCommons Technologies <javacommons@gmail.com>
2022-06-15 14:47:15 +0900

    .

---
+++ babel/traverse.mjs
@@ -0,0 +1,26 @@
+import $parser from "https://jspm.dev/@babel/parser";
+import $traverse from "https://jspm.dev/@babel/traverse";
+import { optimize } from "./optimize.mjs";
+
+const code = `function square(n) {
+  return n * n;
+}`;
+
+let ast = $parser.parse(code);
+
+$traverse.default(ast, {
+  enter(path) {
+    if (path.isIdentifier({ name: "n" })) {
+      path.node.name = "x";
+    }
+  },
+});
+
+$traverse.default(ast, {
+  FunctionDeclaration: function(path) {
+    path.node.id.name = "x";
+  },
+});
+
+ast = optimize(ast);
+console.log(JSON.stringify(ast, null, "  "));
