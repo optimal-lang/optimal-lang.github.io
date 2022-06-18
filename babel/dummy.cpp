0df20b5e6150ab9ed88ce344e7dd598a627dcf32
JavaCommons Technologies <javacommons@gmail.com>
2022-06-18 09:45:26 +0900

    .

---
+++ babel/dummy.cpp
@@ -0,0 +1,25 @@
+#include "omscript.cpp.h"
+
+int main()
+{
+    GC_INIT();
+    static om_register *null = new_null();
+    static om_register *undefined = new_undefined();
+    om_register *x = new_string("abc");
+    do
+    {
+        if (bool_value(equal(x, new_string("a"))))
+        {
+            print(new_string("match-a"));
+            goto label1;
+        }
+        goto label0;
+    label1:
+        print(new_string("a"));
+    label0:
+        ;
+    } while(false);
+    print(new_string("END"));
+
+    return 0;
+}
