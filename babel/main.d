94dfde253fd2a1227356127c9ae692e5fd474581
JavaCommons Technologies <javacommons@gmail.com>
2022-07-01 20:38:46 +0900

    .

--- babel/main.d
+++ babel/main.d
@@ -4,9 +4,12 @@ import omscript;
 void main()
 {
 	print(new_bool(true));
+	print(new_string("abc"));
+	print(new_number(1.23));
 	om_register_ptr_vector vect = new om_register_ptr_vector();
 	vect.push_back(new_bool(true));
 	vect.push_back(new_bool(false));
-	//om_register_ptr list = new_list([new_bool(true), new_bool(false)]);
+	vect.push_back(new_string("abc"));
+	vect.push_back(new_number(1.23));
 	print(new_list(vect));
 }
