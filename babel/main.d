0273a825f491cbe2db6e39c857eff4e04beb1542
JavaCommons Technologies <javacommons@gmail.com>
2022-07-01 21:51:29 +0900

    .

--- babel/main.d
+++ babel/main.d
@@ -6,13 +6,12 @@ void main()
 	print(new_bool(true));
 	print(new_string("abc"));
 	print(new_number(1.23));
-	om_data list1 = () {
-		om_list_data vect = new om_list_data();
-		vect.push_back(new_bool(true));
-		vect.push_back(new_bool(false));
-		vect.push_back(new_string("abc"));
-		vect.push_back(new_number(1.23));
-		return new_list(vect);
+	om_data list1 = (om_list_data __result__ = new om_list_data()) {
+		__result__.push_back(new_bool(true));
+		__result__.push_back(new_bool(false));
+		__result__.push_back(new_string("abc"));
+		__result__.push_back(new_number(1.23));
+		return new_list(__result__);
 	}();
 	print(list1);
 	om_list_data vect = new om_list_data();
