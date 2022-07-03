2415ee4d7549433bf0e3b301a759b6c2d9193ff4
JavaCommons Technologies <javacommons@gmail.com>
2022-07-04 00:40:58 +0900

    .

--- babel/run.sh
+++ babel/run.sh
@@ -1,6 +1,6 @@
 #! bash -uvx
 set -e
-rm -rf omscript_wrap.cxx omscript.d omscript_im.d
+rm -rf omscript_wrap.cxx omscript_wrap.h omscript.d omscript_im.d
 swig -c++ -d -d2 omscript.i
 sed -i -e 's/package struct SwigPendingException/struct SwigPendingException/g' omscript_im.d
 sed -i -e 's/private bool swigIsMethodOverridden(DelegateType, FunctionType, alias fn)() const/private bool swigIsMethodOverridden(DelegateType, FunctionType, alias fn)()/g' omscript.d
