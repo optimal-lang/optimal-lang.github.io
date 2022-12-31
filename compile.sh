26876a697b1dc70fe26a585ba20325e00492eefb
JavaCommons Technologies <javacommons@gmail.com>
2022-12-31 09:46:03 +0900

    .

--- compile.sh
+++ compile.sh
@@ -5,7 +5,8 @@ sleep 3
 deno run --allow-all compile2.mjs
 astyle --style=allman tmp.cpp
 cat tmp.cpp
-mingwx.cmd g++.exe tmp.cpp -static -lgc -lgccpp
+#mingwx.cmd g++.exe tmp.cpp -static -lgc -lgccpp
+g++.exe tmp.cpp -static -lgc -lgccpp
 ./a.exe | tee compile2.txt
 dos2unix compile1.txt compile2.txt
 diff compile1.txt compile2.txt
